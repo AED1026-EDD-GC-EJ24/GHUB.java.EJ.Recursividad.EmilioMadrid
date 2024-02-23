7122dff44aa4c4e5c3d3a634590c4fbe73af1a36
Juan Emilio Madrid Alvarez <158490593+emilioMadrid0408@users.noreply.github.com>
2024-02-23 15:57:54 +0000

    appLadrillos

---
+++ miPrincipal/AppLadrillos.java
@@ -0,0 +1,20 @@
+ package miPrincipal;
+
+public class AppLadrillos {
+    public static void menu() {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXX");
+        }
+
+        
+    }
+    public static void dibujarPared(){
+        dibujarPared();
+    }
+
+    public static void dibujarHilera(){
+        System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX");
+    }
+
+    
+}
