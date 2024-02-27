91eab704334b77a272875f1a350c6494e9a09467
Juan Emilio Madrid Alvarez <158490593+emilioMadrid0408@users.noreply.github.com>
2024-02-27 16:01:17 +0000

    sumadigitos

--- miPrincipal/AppLadrillos.java
+++ miPrincipal/AppLadrillos.java
@@ -3,13 +3,19 @@
 public class AppLadrillos {
     public static void menu() {
         for (int i = 0; i < 10; i++) {
-            System.out.println("XXXXXXXXXXXXXXXXXXXXXX");
+            dibujarHilera();
         }
+        int n =10;
 
-        
+        dibujarPared(n);
     }
-    public static void dibujarPared(){
-        dibujarPared();
+    public static void dibujarPared(int n){
+        if (n<10) {
+            dibujarPared(n+1);
+            System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXX");
+        }
+        dibujarPared(n-1);
+       
     }
 
     public static void dibujarHilera(){
--- miPrincipal/Principal.java
+++ miPrincipal/Principal.java
@@ -3,37 +3,45 @@ package miPrincipal;
 import java.util.Scanner;
 
 public class Principal {
-    
+
     public static void main(String[] args) throws Exception {
         Scanner consola = new Scanner(System.in);
-        int opc =0;
-        do{
+        int opc = 0;
+        do {
             System.out.println("*************************");
             System.out.println("TIPOS ABSTRACTOS DE DATOS");
             System.out.println("*************************");
             System.out.println("1) LADRILLOS");
+            System.out.println("2) FACTORIAL");
+            System.out.println("3) NATURALES");
             System.out.println("        ");
             System.out.println("0) SALIR");
             System.out.print("Selecciona opción:");
-            opc=consola.nextInt();
+            opc = consola.nextInt();
 
-            switch (opc){
-            case 1:
-               AppLadrillos.menu();
-               break;
-               
-            case 0:
-               System.out.println("ADIOS!");
-               consola.close();
-               break;
-               
+            switch (opc) {
+                case 1:
+                    AppLadrillos.menu();
+                    break;
+                case 2:
+                    AppFactorial.menu();
 
-            default:
-               System.out.println("Valor incorrecto, intente de nuevo!");
+                case 3:
+                    AppNaturales.menu();
 
-        }
-       
-    }while (opc != 0);
-		
+                    case 4:
+                    AppSumaDigitas.menu();
+                case 0:
+                    System.out.println("ADIOS!");
+                    consola.close();
+                    break;
+
+                default:
+                    System.out.println("Valor incorrecto, intente de nuevo!");
+
+            }
+
+        } while (opc != 0);
+
     }
 }
